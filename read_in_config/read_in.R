d69ca21435b1baffa7b28bf271b86ceb3e090539
Shan <30907972+ShanzhaoWang@users.noreply.github.com>
2019-01-08 17:31:26 -0800

    Create read_in.R

---
+++ read_in_config/read_in.R
@@ -0,0 +1,11 @@
+# for the template that I use
+for (i in 1:2) {dev_config[,i] <- stringr::str_trim(dev_config[,i])} #str_trim to remove whitespaces
+# remove comments
+indexes_comment <- which(sapply(dev_config[,1], startsWith, "#"))
+if (length(indexes_comment)>0){
+  dev_config <- dev_config[-indexes_comment,]
+}
+dev_config <- data.frame(t(dev_config), stringsAsFactors = FALSE)
+
+names(dev_config) <- as.character(dev_config[1,])
+dev_config <- dev_config[-1,]
