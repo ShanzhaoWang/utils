51d9c83c7bea79db35ccdf2ac0cef73c16b17bdd
Shan <30907972+ShanzhaoWang@users.noreply.github.com>
2019-01-09 11:52:10 -0800

    Update other_funcs_1.R

--- other_funcs/other_funcs_1.R
+++ other_funcs/other_funcs_1.R
@@ -1,4 +1,6 @@
 # list all files in the current directory
 dir()
 # list all files with certain name pattern
-list.files("*.txt")
+files <- list.files("*.txt")
+# read all files ending with .txt and combine them as one single df
+do.call("rbind",lapply(files, FUN=function(x){read.delim(x,stringsAsFactors = FALSE, sep=",")}))
