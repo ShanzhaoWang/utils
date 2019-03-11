824683151dc982b1669362ae501bfa8e2567b516
Shan <30907972+ShanzhaoWang@users.noreply.github.com>
2019-03-11 10:23:43 -0700

    Update read_in.R

--- read_in_config/read_in.R
+++ read_in_config/read_in.R
@@ -17,3 +17,32 @@ dev_config <- dev_config[-1,]
       OS_USERNAME <<- readline(prompt="OVGTB username: ")
       OS_PASSWORD <<- readline(prompt="OVGTB password: ")
     }
+
+### read all sheets in all excel files within a folder with some name requirements
+ottaid_voablock_fromation <- function(w_dir = "/Users/szw/Desktop/nanostring", folder_name) {
+  data_file <- list.files(path = paste0(w_dir, "/", folder_name), pattern = ".xlsx")
+  df <- tibble::data_frame(path = data_file) %>%
+    dplyr::mutate(., path = paste0(w_dir, "/", folder_name, "/", data_file)) %>%
+    dplyr::mutate(., sheet_name = purrr::map(path, readxl::excel_sheets)) %>%
+    tidyr::unnest(.) %>%
+    dplyr::mutate(., data = purrr::map2(
+      path, sheet_name,
+      ~readxl::read_excel(.x, .y)))
+  
+  # store list of data frames in df_b
+  df_b <- df$data
+  
+  # convert df_b into a dataframe by combining all dataframes with columns "OTTA ID" and "BLOCK ID"
+  df_b <- do.call("rbind", lapply(df_b, function(x) {
+    if(all(c("OTTA ID", "BLOCK ID") %in% colnames(x))) {
+      col_selected <- x %>% 
+      dplyr::select(., c("OTTA ID", "BLOCK ID"))
+      return(col_selected)
+    }
+  }))
+  
+  # filter out all TVAN2 from the file in data1 folder
+  df_b <- df_b[which(stringr::str_detect(df_b$`OTTA ID`, "TVAN2")), ] %>% unique(.)
+  return(df_b)
+}  
+
